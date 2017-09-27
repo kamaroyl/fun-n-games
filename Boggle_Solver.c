da3f180baadee4b003ac875529a314a9f5f88195
Benjamin Straub <benjamin_straub@comcast.com>
2017-09-27 14:11:13 -0600

    made a few changes based on a c++ trie implementation of the boggle board solution, now pulls file into memory and works off of that to save file io calls. program now reads in file and determines the size of the board and allocates required memory. Tested and works on 8x8, fails on 100x100, so I need to do some work on the file parsing/memory allocation

--- Boggle_Solver.c
+++ Boggle_Solver.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <memory.h>
 #include <time.h>
+#include <stdlib.h>
 
 /*
   * Constants
   * ****************
   *
-  * N - Dimension of the boggle board
-  *
   * minimumWordLength - boggle has a minumum work length of 3, any word shorter than 3 is worth 0 points
-  *
-  * N_sq - save the square of the number since it occurs to often
   *
   * dx and dy - representation of the x and y coordinates of the 3x3 kernel
   *
@@ -20,8 +17,6 @@
   *
   * dictionaryFileName - the full path to the dictionary file
   * boardFileName - the full path to the board file
-  * readTypeText - the string required for file reading
-  *
   *
   *
   *  BOARD EXAMPLE:
@@ -34,18 +29,46 @@
   *          EFGHIJ
   *
   *  Dictionary is all in CAPS to board has to be as well.
-  *  Dictionary is a word per line
   */
-const int N = 6;
 const int minimumWordLength = 3;
-const int N_sq = N*N;
 const int dx[] = {0,1,1,1,0,-1,-1,-1};
 const int dy[] = {1,1,0,-1,-1,-1,0,1};
 const int lengthOfKernel = 8;
-const char *dictionaryFileName = "";
-const char *boardFileName = "";
-const char *readTypeText = "r";
+const char *dictionaryFileName = "/Users/bstrau202/CLionProjects/untitled1/dict.txt";
+const char *boardFileName = "/Users/bstrau202/CLionProjects/untitled2/boggle.txt";
+int count=0;
 
+
+//https://github.com/machineboy2045/fast-boggle-solver/blob/master/boggle_solver.cpp
+char* readF( const char fname[] ){
+    FILE * pFile;
+    long lSize;
+    char * buffer;
+    size_t result;
+
+    pFile = fopen ( fname , "r" );
+    if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
+
+    // obtain file size:
+    fseek (pFile , 0 , SEEK_END);
+    lSize = ftell (pFile);
+    rewind (pFile);
+
+    // allocate memory to contain the whole file:
+    buffer = (char*) malloc (sizeof(char)*lSize);
+    if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
+
+    // copy the file into the buffer:
+    result = fread (buffer,1,lSize,pFile);
+    if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
+
+    /* the whole file is now loaded in the memory buffer. */
+
+    // terminate
+    fclose (pFile);
+    // free (buffer);
+    return buffer;
+}
 
 
 /**
@@ -54,20 +77,15 @@ const char *readTypeText = "r";
  *
  * currentWord - storage for words from the dictionary - size limited to the number of characters on the board + 1
  * board - storage for the boggle board
- * previousString - storage of the previous string for
  * memoizationTable -
  * visited - table of
  *
  */
-char currentWord[N_sq+1];
-char board[N+1][N+2];
-char previousString[N_sq+1];
-char memoizationTable[N_sq+1][N][N];
-char visited[N][N];
+
+
 int previousWordLength=0;
 
-
-char checkBoard(char* word, int curIndex, int row, int column, int wordLen)
+char checkBoard(char* word, int curIndex, int row, int column, int wordLen, int dimension, char visited[dimension][dimension],char board[dimension+1][dimension+2])
 {
     char ret = 0;
     if (curIndex == wordLen - 1) {
@@ -76,14 +94,13 @@ char checkBoard(char* word, int curIndex, int row, int column, int wordLen)
     for (int m = 0; m < lengthOfKernel; m++){
         int newRow = row + dx[m];
         int newColumn = column + dy[m];
-        if (newRow >= 0 && newRow < N &&
-            newColumn >= 0 && newColumn < N &&
+        if (newRow >= 0 && newRow < dimension &&
+            newColumn >= 0 && newColumn < dimension &&
             !visited[newRow][newColumn] &&
             word[curIndex+1] == board[newRow][newColumn]){
-
             ++curIndex;
             visited[newRow][newColumn] = 1;
-            ret = checkBoard(word, curIndex, newRow, newColumn, wordLen);
+            ret = checkBoard(word, curIndex, newRow, newColumn, wordLen, dimension, visited, board);
             if (ret) break;
             --curIndex;
             visited[newRow][newColumn] = 0;
@@ -93,39 +110,62 @@ char checkBoard(char* word, int curIndex, int row, int column, int wordLen)
     return ret;
 }
 
+
 /*
  * Based off the solution and code proposed by Golam Kawsar
  * http://exceptional-code.blogspot.com/2012/02/solving-boggle-game-recursion-prefix.html
  */
 int openBoard() {
-    FILE *boardFile = fopen(boardFileName, readTypeText);
-    FILE *dictionaryFile = fopen(dictionaryFileName, readTypeText);
-    int rc, firstMismatch;
-    int i = 0,j = 0, k = 0, l = 0;
-    for(i = 0; i < N; ++i) {
-        fgets(board[i], N + 2, boardFile);
+    //Initialize variables
+
+    int i = 0, j = 0, k = 0, l = 0;
+    int firstMismatch;
+    char* boardBuffer = readF(boardFileName);
+    printf("size of boardBuffer: %d", (int)sizeof(boardBuffer));
+    char* currentLine = strtok(boardBuffer,"\n\t");
+    char* previousWord  = "";
+    char* lastFoundWord = "";
+    int dimension = strlen(currentLine);
+    int dimension_squared = dimension*dimension;
+    char board[dimension+1][dimension+2];
+    char memoizationTable[dimension_squared+1][dimension][dimension];
+    char visited[dimension][dimension];
+
+
+    //Build Board
+    while(currentLine!=NULL){
+        for(j=0; j<dimension; j++)
+            board[i][j]=currentLine[j];
+        i++;
+        currentLine = strtok (NULL, "\n");
     }
-    for(i=0;i<N; i++){
-        for(j=0; j<N; j++){
-            if(j%N==0){
+    for(i=0;i<dimension;i++){
+        for(j=0; j<dimension; j++){
+            if(j%dimension==0){
                 printf("\n");
             }
             printf("%c ", board[i][j]);
         }
     }
     printf("\n");
+    free(currentLine);
+    free(boardBuffer);
+    char* dictionaryBuffer = readF(dictionaryFileName);
+    char* currentWord = strtok(dictionaryBuffer,"\n\t");
 
     /*
      * Main loop
      */
-    while(fgets(currentWord, N_sq+1, dictionaryFile)) {
+
+    while(currentWord!=NULL) {
         int wordLength = strlen(currentWord);
         char flag = 0;
         /*
          * Remove trailing newline with NULL
          */
+
         if (currentWord[wordLength-1] == '\n'){
-            currentWord[wordLength-1] = NULL;
+            currentWord[wordLength-1] = '\0';
             --wordLength;
         }
 
@@ -133,6 +173,7 @@ int openBoard() {
          * If the word length is less than the minimum word length, skip the word
          */
         if(wordLength<minimumWordLength){
+            currentWord = strtok (NULL, "\n\t");
             continue;
         }
 
@@ -140,7 +181,7 @@ int openBoard() {
          * Check for the index of the first different character
          */
         for(i = 0; i < wordLength && i < previousWordLength; ++i){
-            if(previousString[i] != currentWord[i])
+            if(previousWord[i] != currentWord[i])
                 break;
         }
 
@@ -151,8 +192,8 @@ int openBoard() {
          * check the rest of the board for the starting character of the word
          */
         if(firstMismatch==0){
-            for(i = 0; i < N; ++i){
-                for(j = 0; j < N; ++j){
+            for(i = 0; i < dimension; ++i){
+                for(j = 0; j < dimension; ++j){
                     if(board[i][j] == currentWord[0]) {
                         memoizationTable[0][i][j] = 1;
                     }
@@ -168,11 +209,10 @@ int openBoard() {
          * Check the board from the first mismatched character through the number of
          */
         for(k = firstMismatch; k < wordLength; ++k) {
-            for (i = 0; i < N; ++i) {
-                for (j = 0; j < N; ++j) {
+            for (i = 0; i < dimension; ++i) {
+                for (j = 0; j < dimension; ++j) {
 
                     memoizationTable[k][i][j] = 0;
-
                     if (board[i][j] != currentWord[k]) {
                         continue;
                     }
@@ -180,7 +220,7 @@ int openBoard() {
                     for (l = 0; l < lengthOfKernel && !memoizationTable[k][i][j]; ++l) {
                         int ti = i + dx[l];
                         int tj = j + dy[l];
-                        if (ti < 0 || ti >= N || tj < 0 || tj >= N){
+                        if (ti < 0 || ti >= dimension || tj < 0 || tj >= dimension){
                             continue;
                         }
                         if (memoizationTable[k - 1][ti][tj] == 1) {
@@ -192,10 +232,10 @@ int openBoard() {
         }
 
         /*
-         * for each entry in the memoization table at wordlength-1 check if true
+         * for each entry in the memoization table at wordLength-1 check if true
          */
-        for(i = 0; i < N && !flag; ++i){
-            for(j = 0; j < N && !flag; ++j){
+        for(i = 0; i < dimension && !flag; ++i){
+            for(j = 0; j < dimension && !flag; ++j){
                 if(memoizationTable[wordLength-1][i][j]){
                     flag=1;
                 }
@@ -206,17 +246,22 @@ int openBoard() {
          * if memoization table was true, then check the word against the board
          */
         if(flag){
-            for (i = 0; i < N; ++i){
-                for (j = 0; j < N; ++j){
+            for (i = 0; i < dimension; ++i){
+                for (j = 0; j < dimension; ++j){
                     if (currentWord[0] != board[i][j]){
                         continue;
                     }
                     memset(visited, 0, sizeof(visited));
-
                     visited[i][j] = 1;
 
-                    if (checkBoard(currentWord, 0, i, j, wordLength)){
-                        printf("%s found!\n", currentWord);
+                    if (checkBoard(currentWord, 0, i, j, wordLength, dimension, visited, board)){
+                        if(strcmp(lastFoundWord, currentWord)==0){
+                            break;
+                        }
+                        lastFoundWord=currentWord;
+                        printf("%s\n", currentWord);
+                        count++;
+
                         break;
                     }
                 }
@@ -226,20 +271,22 @@ int openBoard() {
         /*
          * Set previous string
          */
-        strcpy(previousString, currentWord);
+        previousWord = currentWord;
         previousWordLength = wordLength;
-        //printf("%s\n", currentWord);
+        currentWord = strtok (NULL, "\n\t");
+
     }
-    fclose( dictionaryFile );
-    fclose( boardFile );
+    free(dictionaryBuffer);
     return 0;
 }
 
 
 int main(){
+
     clock_t begin = clock();
     openBoard();
     clock_t end = clock();
-    printf("%f", (double) (end-begin)/CLOCKS_PER_SEC );
+    printf("Time took to run: %f\n", (double) (end-begin)/CLOCKS_PER_SEC );
+    printf("Found %d words", count);
     return 0;
 }
